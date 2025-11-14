a37a59bbd45342f0b5a5fc93b166473f2994bf54
Julia Jimenez <jjimenezperez@celestica.com>
2025-11-14 20:35:16 +0100

    Cambios en pagina rpincipal

--- js/script_julia.js
+++ js/script_julia.js
@@ -1,27 +1,36 @@
-// Usando const para una función que no se reasignará [cite: 2863]
-const cambiarTexto = () => {
-    // 1. Seleccionar el elemento dinámico por su ID [cite: 1822]
-    let parrafo = document.getElementById('parrafo-dinamico');
-    
-    // 2. Comprobar su contenido para alternar el estado (lógica simple con if/else) [cite: 1640]
-    if (parrafo.textContent === "¡Contenido y estilo cambiados con éxito!") {
-        
-        // Vuelve al estado original
-        parrafo.textContent = "Este párrafo volverá al contenido original.";
-        parrafo.style.color = "black"; // Manipulación de estilos con sintaxis JavaScript [cite: 6030]
-        parrafo.style.fontSize = "16px";
-        
-    } else {
-        
-        // Cambia el contenido y estilo
-        parrafo.textContent = "¡Contenido y estilo cambiados con éxito!"; // Manipulación de contenido [cite: 5924]
-        parrafo.style.color = "blue"; 
-        parrafo.style.fontSize = "24px";
+function MostrarTodos() {
+    let animales = document.querySelectorAll('#contenedor-animales > div');
+    for (let i = 0; i < animales.length; i++) {
+        let animal = animales[i];
+        animal.style.display = 'block'; //Muestra el elemento
+    }
+}
+function OcultarTodos() {
+    // Selecciona todos los divs descendientes del padre "contenedor-animales""
+    let todosLosAnimales = document.querySelectorAll('#contenedor-animales > div');
+
+    for (let i = 0; i < todosLosAnimales.length; i++) {
+        let animal = todosLosAnimales[i];
+        animal.style.display = 'none';         // Oculta el elemento.
+    }
+}
+function FiltrarOviparos() {
+    OcultarTodos();
+    let animales_oviparos = document.querySelectorAll('.oviparo'); // Selecciono todos los elementos que tengan clase "oviparo" > Para clases uso el punto.
+                                                                                                                              //> Para id uso el #.
+    for (let i = 0; i < animales_oviparos.length; i++) {
+        let animal = animales_oviparos[i];
+        animal.style.display = 'block';
     }
 }
 
-// Se puede añadir un escuchador de evento al cargar la página si la función es anónima o una referencia 
-// document.addEventListener('DOMContentLoaded', function() {
-//     let boton = document.getElementById('boton-interactivo');
-//     boton.addEventListener('click', cambiarTexto);
-// });
+function FiltrarMamiferos(){
+    OcultarTodos();
+    let animales_mamiferos = document.querySelectorAll('.mamifero'); // Selecciono todos los elementos que tengan clase "mamifero"
+    for (let i = 0; i < animales_mamiferos.length; i++) {
+        let animal = animales_mamiferos[i];
+        animal.style.display = 'block';
+    }
+}
+
+//document.addEventListener('DOMContentLoaded', MostrarTodos);
