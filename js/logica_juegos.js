432de6a0fb76ac426776df0284a74f160c523f9d
mmoreno <mmoreno@power-electronics.com>
2025-12-12 13:44:45 +0100

    carga y estilo del tablero

--- js/logica_juegos.js
+++ js/logica_juegos.js
@@ -1,6 +1,13 @@
-// Funcion para abrir un juego desde la galería
-function abrirJuego(juegoId) {
 
+/**
+  Author:       M.Moreno
+  Date:         Dec.25
+  Description:  Lógica de juego para 3 en raya.
+**/
+
+// Se ejecuta al pulsar "Jugar Ahora" en la tarjeta
+function abrirJuego(juegoId) {
+    
     const vistaSeleccion = document.getElementById('vista-seleccion');
     if (vistaSeleccion) {
         vistaSeleccion.classList.add('oculto');
@@ -12,18 +19,48 @@ function abrirJuego(juegoId) {
     }
 }
 
-// Funcion para volver a la galería desde un juego
-function volverGaleria() {
+// Se ejecuta al pulsar "Comenzar Juego" dentro de la intro del 3 en Raya
+function iniciarPartidaUI() {
     
+    const intro = document.getElementById('intro-juego');
+    if (intro) {
+        intro.classList.add('oculto');
+    }
+
+    // Mostramos el tablero de juego
+    const tablero = document.getElementById('tablero-container');
+    if (tablero) {
+        tablero.classList.remove('oculto');
+    }
+    
+    console.log("Tablero cargado. Aquí irá la lógica del juego más adelante.");
+}
+
+// Se ejecuta al pulsar "Volver" o "Salir al menú"
+function volverGaleria() {
+
     const contenedorJuego = document.getElementById('contenedor-juego-3enRaya');
-    if (contenedorJuego) contenedorJuego.classList.add('oculto');
+    if (contenedorJuego) {
+        contenedorJuego.classList.add('oculto');
+    }
 
     const vistaSeleccion = document.getElementById('vista-seleccion');
     if (vistaSeleccion) {
         vistaSeleccion.classList.remove('oculto');
     }
 
-    //Reset visual
-    document.getElementById('intro-juego').classList.remove('oculto');
-    document.getElementById('tablero-container').classList.add('oculto');
+    // Reset visual
+    const introJuego = document.getElementById('intro-juego');
+    const tableroContainer = document.getElementById('tablero-container');
+
+    if (introJuego) introJuego.classList.remove('oculto');      
+    if (tableroContainer) tableroContainer.classList.add('oculto'); 
 }
+
+
+/***************************************************************************************** */
+/**
+  Author:       M.Ripoll
+  Date:         Dec.25
+  Description:  Lógica para Quiz.
+**/
