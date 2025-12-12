1a79ad8d300ff76c2c96476bb7d92fa75a965f85
mmoreno <mmoreno@power-electronics.com>
2025-12-12 18:34:43 +0100

    incializo servidor

---
+++ server.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    
+    res.sendFile(path.join(__dirname, 'index_julia.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en: http://localhost:${PORT}`);
+});
